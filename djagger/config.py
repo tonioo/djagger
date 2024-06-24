bc03f2d004d68c6b282289b39f87b98ce77b5c6d
Antoine Nguyen <tonio@ngyn.org>
2024-06-24 14:29:07 +0200

    Fixed compat issue with Pydantic 2.7

--- djagger/config.py
+++ djagger/config.py
@@ -1,9 +1,10 @@
+from typing import ClassVar
 from django.core.exceptions import ImproperlyConfigured
 from pydantic import BaseModel
 
 class DjaggerConfig(BaseModel):
     """Djagger configuration schema"""
-    global_prefix = ""
+    global_prefix: ClassVar[str] = ""
 
 try:
     from django.conf import settings
